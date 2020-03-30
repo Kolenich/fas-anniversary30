6648e0247c6c134c87499120837ddec5ed22311c
Nikolay <nick.zhigalin@gmail.com>
2020-03-30 16:59:56 +0300

    Ссылки в навбаре передаются через контекст

--- content/views.py
+++ content/views.py
@@ -14,6 +14,18 @@ class IndexView(ListView):
 
     def get_context_data(self, **kwargs):
         """Переопределение метода передачи в шаблон контекста. Добавляет остальные кверисеты по моделям."""
+        navbar_links = [
+            {'name': 'Обращение', 'width': '126px', 'href': '#appeal'},
+            {'name': 'Хроника', 'width': '111px', 'href': '#chronicle'},
+            {'name': 'История', 'width': '103px', 'href': '#history'},
+            {'name': '10 законов', 'width': '123px', 'href': '#ten-laws'},
+            {'name': 'Прямая речь', 'width': '134px', 'href': '#direct-speech'},
+            {'name': 'Видео поздравления', 'width': '189px', 'href': '#video-congratulation'},
+            {'name': 'Итоги', 'width': '80px', 'href': '#summary'},
+            {'name': 'Музей', 'width': '83px', 'href': '#museum-materials-reports'},
+            {'name': 'Материалы', 'width': '117px', 'href': '#museum-materials-reports'},
+            {'name': 'Доклады', 'width': '104px', 'href': '#museum-materials-reports'},
+        ]
         context = super().get_context_data(**kwargs)
         context.update({
             'books': Book.objects.all(),
@@ -22,5 +34,6 @@ class IndexView(ListView):
             'photo_cards': PhotoCard.objects.all(),
             'infographics': Infographics.objects.all(),
             'magazines': Magazine.objects.all(),
+            'navbar_links': navbar_links
         })
         return context
