e74eb45fe312b62264b60839d1e5544988b24297
Nikolay <nick.zhigalin@gmail.com>
2020-03-30 18:22:02 +0300

    Дополнительный контекст (не модели) вынесен в отдельный файл с автоимпортом

---
+++ content/extra_context.py
@@ -0,0 +1,14 @@
+"""Дополнительные переменные для контекста шаблона."""
+
+navbar_links = [
+    {'name': 'Обращение', 'width': '126px', 'href': '#appeal'},
+    {'name': 'Хроника', 'width': '111px', 'href': '#chronicle'},
+    {'name': 'История', 'width': '103px', 'href': '#history'},
+    {'name': '10 законов', 'width': '123px', 'href': '#ten-laws'},
+    {'name': 'Прямая речь', 'width': '134px', 'href': '#direct-speech'},
+    {'name': 'Видео поздравления', 'width': '189px', 'href': '#video-congratulation'},
+    {'name': 'Итоги', 'width': '80px', 'href': '#summary'},
+    {'name': 'Музей', 'width': '83px', 'href': '#museum-materials-reports'},
+    {'name': 'Материалы', 'width': '117px', 'href': '#museum-materials-reports'},
+    {'name': 'Доклады', 'width': '104px', 'href': '#museum-materials-reports'},
+]
