1778a199c1d920714e284c6064515da0e945431a
Nikolay <nick.zhigalin@gmail.com>
2020-03-30 14:35:39 +0300

    РАсширен передаваемый в шаблон контекст

--- content/views.py
+++ content/views.py
@@ -2,12 +2,25 @@
 
 from django.views.generic import ListView
 
-from content.models import Book
+from content.models import Book, Booklet, Document, Infographics, Magazine, PhotoCard, Video
 
 
 class IndexView(ListView):
     """Корневое представление главной страницы."""
 
     template_name = 'index.html'
-    model = Book
-    context_object_name = 'books'
+    model = Booklet
+    context_object_name = 'booklets'
+
+    def get_context_data(self, **kwargs):
+        """Переопределение метода передачи в шаблон контекста. Добавляет остальные кверисеты по моделям."""
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'books': Book.objects.all(),
+            'videos': Video.objects.all(),
+            'documents': Document.objects.all(),
+            'photo_cards': PhotoCard.objects.all(),
+            'infographics': Infographics.objects.all(),
+            'magazines': Magazine.objects.all(),
+        })
+        return context
