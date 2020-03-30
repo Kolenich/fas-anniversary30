a5495d9215fab79ef11f28504816afb7c424d3d9
Nikolay <nick.zhigalin@gmail.com>
2020-03-30 23:37:37 +0300

    Убрана лишняя прослойка урлов

--- anniversary/urls.py
+++ anniversary/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+
+from content.views import IndexView
 
 urlpatterns = [
-    path('', include('content.urls')),
+    path('', IndexView.as_view(), name='home'),
     path('admin/', admin.site.urls),
 ]
 
